9b4d1a2e9267f19266299f01a0c7144e4433d586
Chris Crow and Skye Nguyen <chris@>
2018-08-16 16:46:25 -0700

    make final tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 function encoder(input) {
   var lowerCaseString = input.toLowerCase();
   var alphaString = lowerCaseString.replace(/\W/g, "");
-  console.log(alphaString.length);
-  var rowNumber = Math.floor(Math.sqrt(alphaString.length));
+  console.log (alphaString.length);
+  var rowNumber = Math.ceil(Math.sqrt(alphaString.length));
+  console.log(rowNumber);
   var alphaArray = alphaString.split("");
   var codedArray = []
-  console.log(rowNumber)
 
   for (loop = 0; loop < rowNumber; loop++) {
     for (var i = 0; i < alphaArray.length; i += rowNumber) {
       codedArray.push(alphaArray[i + loop])
     }
   }
-  for (i = 5; i < codedArray.length; i += 6) {
-    codedArray.splice(i, 0, " ");
-  }
-
-  var result = codedArray.join("");
-  return result;
+  var codedString = codedArray.join("");
+  var result = codedString.replace(/(\w{5})/g, "$1 ")
+  return result
 }
 
 

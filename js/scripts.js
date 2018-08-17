eb880ad21053c713afd519fb8b2d462f6aaa2be8
Chris Crow <chrismcrow@gmail.com>
2018-08-16 20:52:08 -0700

    guarantee the correct amount of rows and columns.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,27 @@ function encoder(input) {
   var lowerCaseString = input.toLowerCase();
   var alphaString = lowerCaseString.replace(/\W/g, "");
   console.log (alphaString.length);
-  var rowNumber = Math.ceil(Math.sqrt(alphaString.length));
-  console.log(rowNumber);
+  var row = Math.ceil(Math.sqrt(alphaString.length));
+  if (Math.sqrt(alphaString.length) < row) {
+    column = row - 1
+  } else {
+    column = row
+  }
+  console.log(row);
+  console.log(column);
   var alphaArray = alphaString.split("");
   var codedArray = []
 
-  for (loop = 0; loop < rowNumber; loop++) {
-    for (var i = 0; i < alphaArray.length; i += rowNumber) {
+  for (loop = 0; loop < column; loop++) {
+    for (var i = 0; i < alphaArray.length; i += column) {
       codedArray.push(alphaArray[i + loop])
     }
   }
+  console.log(codedArray);
   var codedString = codedArray.join("");
-  var result = codedString.replace(/(\w{5})/g, "$1 ")
-  return result
+  var result = codedString.replace(/(\w{5})/g, "$1 ");
+  console.log(result)
+  return result;
 }
 
 
